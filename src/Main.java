297b63b08ed9e59a0819f7ceda60794d26e88404
Daniel Xavier <danieelxavier@gmail.com>
2016-01-06 14:44:34 -0400

    calclando o tempo e tratamento de exceção para caminho dos dados externos errados

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -15,39 +17,51 @@ public class Main {
 	static FileWriter arq; 
 	static PrintWriter gravarArq;
 	
+	static long tempoInicial;
+	static long tempoAtual;
+	static Double tempoResult;
+	
 	public static void main(String[] args) throws IOException{
 		// TODO Auto-generated method stub
 		
-		System.out.println("Extraindo dados ...");
+		tempoInicial = System.currentTimeMillis();
 		
-		String arg = args[0];
+		String arg = "/home/daniel/Área de Trabalho/docs";
 		
 		if(!arg.endsWith("/"))
 			arg = arg + "/";
 		
 
-		documentos = Extrator.extrairDocumentos(arg); //extrai os dados dos documentos do arquivo txt
-		consultas = Extrator.extrairConsultas(arg); //extrai os dados das consultas do arquivo txt
-		listaInvertidaDocumentos = Extrator.getListaInvertida(documentos); //extrai todos os termos dos documentos, calculando TF e IDF
 		
-		System.out.println("Extração Finalizada");
-		
-		arq = new FileWriter("resultados.txt");
-		gravarArq = new PrintWriter(arq);
-		
-		gravarArq.printf("--------------------> RESULTADOS DAS CONSULTAS <--------------------\n\n\n");
-		
-		//executa as consultas
-		for (Consulta c : consultas) {
-			processa(c);
+		if (verificaDados(arg)){
+			System.out.println("Extraindo dados ...");
+			
+			documentos = Extrator.extrairDocumentos(arg); //extrai os dados dos documentos do arquivo txt
+			consultas = Extrator.extrairConsultas(arg); //extrai os dados das consultas do arquivo txt
+			listaInvertidaDocumentos = Extrator.getListaInvertida(documentos); //extrai todos os termos dos documentos, calculando TF e IDF
+			
+			tempoAtual = System.currentTimeMillis();
+			System.out.println("Extração Finalizada em " + timeToSeconds(tempoAtual, tempoInicial) + " segundos.");
+			
+			arq = new FileWriter("resultados.txt");
+			gravarArq = new PrintWriter(arq);
+			
+			gravarArq.printf("--------------------> RESULTADOS DAS CONSULTAS <--------------------\n\n\n");
+			
+			//executa as consultas
+			for (Consulta c : consultas) {
+				processa(c);
+			}
+			
+			tempoAtual = System.currentTimeMillis();
+			gravarArq.printf("\n\t\t Processo finalizado em " + timeToSeconds(tempoAtual, tempoInicial) + " segundos.");
+			arq.close();
+			
+			
+			java.awt.Desktop.getDesktop().open( new File( "resultados.txt" ) );
+			System.out.println("Finalizado");
+			System.out.println("Processo finalizado em " + timeToSeconds(tempoAtual, tempoInicial) + " segundos.");
 		}
-		
-		
-		arq.close();
-		
-		
-		java.awt.Desktop.getDesktop().open( new File( "resultados.txt" ) );
-		System.out.println("Finalizado");
 		
 		
 	}
@@ -55,8 +69,10 @@ public class Main {
 	
 	static void processa(Consulta consulta){
 		
+		long tempo = System.currentTimeMillis();
+		
 		System.out.println("processando query " + consulta.getQueryNumber() + "...");
-		gravarArq.printf("Consulta " + consulta.getQueryNumber());
+		gravarArq.printf("CONSULTA " + consulta.getQueryNumber());
 		gravarArq.printf("\n");
 		
 		consulta.processaConsulta(listaInvertidaDocumentos); //gera o ranking de similaridade para a consulta
@@ -70,8 +86,43 @@ public class Main {
 		
 		gravarArq.printf("\nP@10 : " + Medidas.precisaoN(consulta, 10)); //Calcula a precição em N de uma consulta
 		gravarArq.printf("\nMAP : " + Medidas.MAP(consulta)); //Calcula o MAP de uma consulta
+		
+		tempoAtual = System.currentTimeMillis();
+		
+		gravarArq.printf("\n\nProcessamento realizado em " + timeToSeconds(tempoAtual, tempo) + " segundos.");
 		gravarArq.printf("\n\n------------------------------------------\n\n");
 	}
 	
+	static Double timeToSeconds(long a, long b){
+		Double x  = (double) a;
+		Double y  = (double) b;
+		
+		return (x - y)/1000;
+	}
+	
+	static boolean verificaDados(String path){
+
+		File file1 = new File(path);
+		if(!file1.exists()){
+			System.out.println("o diretório \'" + path + "\' não foi encontrado");
+			return false;
+		}
+		
+		for(int i=74; i < 80; i++){
+			File f = new File(path + "cf" + i);
+			if(!f.exists()){
+				System.out.println("O arquivo \'cf" + i + "\' não foi encontrado no diretório \'" + path + "\'");
+				return false;
+			}
+		}
+		
+		File file2 = new File(path + "cfquery");
+		if(!file2.exists()){
+			System.out.println("O arquivo \'cfquery\' não foi encontrado no diretório \'" + path + "\'");
+			return false;
+		}
+		
+		return true;
+	}
 	
 }
